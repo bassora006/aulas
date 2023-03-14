416acc04183443a0d08743fa62b2ec91a9bbac4a
Bassora <125940630+bassora006@users.noreply.github.com>
2023-03-14 07:50:59 -0300

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentAPI
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public int Idade { get; set; }
+
+        public string Cidade { get; set; } = string.Empty;
+
+
+    }
+}
